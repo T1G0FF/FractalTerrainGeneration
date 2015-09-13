f205a7f6a0f80e753d49510c6a69b01822cb5d2e
Thomas <thomas.gitcommit@outlook.com>
2015-09-14 09:59:10 +1000

    Added Documentation.

--- FractalTerrainGen/Terrain.cs
+++ FractalTerrainGen/Terrain.cs
@@ -2,14 +2,40 @@
 
 namespace FractalTerrainGen
 {
+    /// <summary>
+    /// Terrain class, used by the ASCIIMap class to describe individual tiles.
+    /// </summary>
     class Terrain
     {
+        /// <summary>
+        /// The name of the terrain type.
+        /// </summary>
         public string Name { get; private set; }
+        /// <summary>
+        /// The symbol used to draw the terrain.
+        /// </summary>
         public char Symbol { get; private set; }
+        /// <summary>
+        /// The elevation the terrain type appears at.
+        /// </summary>
         public byte Elevation { get; private set; }
+        /// <summary>
+        /// The foreground colour of the terrain symbol.
+        /// </summary>
         public ConsoleColor Fore { get; private set; }
+        /// <summary>
+        /// The background colour of the terrain symbol.
+        /// </summary>
         public ConsoleColor Back { get; private set; }
 
+        /// <summary>
+        /// Terrain type constructor.
+        /// </summary>
+        /// <param name="name">The name of the terrain type.</param>
+        /// <param name="symbol">The symbol used to draw the terrain.</param>
+        /// <param name="elevation">The elevation the terrain type appears at.</param>
+        /// <param name="fore">The foreground colour of the terrain symbol.</param>
+        /// <param name="back">The background colour of the terrain symbol.</param>
         public Terrain(string name, char symbol, byte elevation, ConsoleColor fore, ConsoleColor back)
         {
             Name = name;
@@ -19,6 +45,10 @@ namespace FractalTerrainGen
             Back = back;
         }
 
+        /// <summary>
+        /// Sets the console's foreground and background colour then returns the symbol.
+        /// </summary>
+        /// <returns>The symbol used to draw the terrain.</returns>
         public override string ToString()
         {
             Console.ForegroundColor = Fore;
